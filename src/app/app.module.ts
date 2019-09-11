b8c3f3566eb6c323d9d2a11f7ecce5667b439f99
Mutualewis <mutualewis@gmail.com>
2019-09-11 15:34:48 +0300

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, CommonModule  ],
+  declarations: [ AppComponent, HelloComponent, TodosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
